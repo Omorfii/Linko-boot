47905ec943c8548e4953ff1089110a62d246cd8f
Zoe <caramel.moustache@orange.fr>
2026-06-02 13:22:59 +0200

    start of observability

--- main.go
+++ main.go
@@ -39,6 +39,7 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 	<-ctx.Done()
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	fmt.Printf("Linko is shutting down\n")
 
 	if err := s.shutdown(shutdownCtx); err != nil {
 		fmt.Fprintf(os.Stderr, "failed to shutdown server: %v\n", err)
